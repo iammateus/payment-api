3779db0d67303b972b4243123c1508064d7887ca
Mateus Soares <mateus.silva@pravaler.com.br>
2020-07-17 17:23:23 -0300

    Cleaner code and comments

--- app/Services/PaymentService.php
+++ app/Services/PaymentService.php
@@ -6,14 +6,23 @@ use App\Classes\ItemList;
 
 class PaymentService extends Controller
 {
+    /**
+     * Parsing default params of a payment request to Pagseguro
+     *
+     * @param array $options Internal service formatted data
+     * @return array Pagseguro formatted data
+     */
     public function parseDefaultPaymentParams (array $options) {
-        return [
+        
+        $parsed = [
             'paymentMode' => $options['mode'],
             'currency' => $options['currency'],
             'notificationURL' => $options['notificationURL'],
             'senderName' => $options['sender']['name'],
             'senderCPF' => $options['sender']['document']['value'],
         ];
+        
+        return $parsed;
     }
 
     public function parseItems (ItemList $items): array
