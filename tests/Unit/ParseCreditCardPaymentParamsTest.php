b0a1912dde60512a7390e656e14cd03a1df28643
Mateus Soares <mateussoaress1997@gmail.com>
2020-10-19 11:25:03 -0300

    Parsing credit card token to pagseguro format

--- tests/Unit/ParseCreditCardPaymentParamsTest.php
+++ tests/Unit/ParseCreditCardPaymentParamsTest.php
@@ -5,10 +5,34 @@ use App\Services\PaymentService;
 
 class ParseCreditCardPaymentParamsTest extends TestCase
 {
-    public function testParseCreditCardPaymentParamsTest()
+    /**
+     * Fake a valid options array to be parsed
+     */
+    public function fakeOptions()
     {
+        $faker = Faker::create('pt_BR');
+
+        return [
+            'creditCard' => [
+                'token' => $faker->text()
+            ]
+        ];
+    }
+
+    public function testParseCreditCardPaymentParams()
+    {
+        $options = $this->fakeOptions();
         $mock = Mockery::mock(PaymentService::class)->makePartial();
-        $result = $mock->parseCreditCardPaymentParams();
+        $result = $mock->parseCreditCardPaymentParams($options);
         $this->assertIsArray($result);
+    }
+
+    public function testParseCreditCardPaymentParamsParsingCreditCardToken()
+    {
+        $options = $this->fakeOptions();
+        $mock = Mockery::mock(PaymentService::class)->makePartial();
+        $result = $mock->parseCreditCardPaymentParams($options);
+        $this->assertIsArray($result);
+        $this->assertEquals($result['creditCardToken'], $options['creditCard']['token']);
     }
 }
